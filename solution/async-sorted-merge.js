a1364e237f75cb30c5114b7ff7af8fe3d497e378
traysonkelii <traysonkelii@gmail.com>
2024-09-20 13:53:37 -0700

    solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,71 @@
-"use strict";
+// "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+// const MinHeap = require("../lib/min-heap");
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+// // module.exports = async (logSources, printer) => {
+// //   const heap = new MinHeap((a, b) => a.log.date - b.log.date);
+// //   const initialPromises = [];
+
+// //   // Initialize the heap with the first log from each source
+// //   logSources.forEach((source, index) => {
+// //     const promise = source.popAsync().then((log) => {
+// //       if (log) {
+// //         heap.push({ log, sourceIndex: index });
+// //       }
+// //     });
+// //     initialPromises.push(promise);
+// //   });
+
+// //   // Wait for all initial logs to be fetched
+// //   await Promise.all(initialPromises);
+
+// //   // Merge logs
+// //   while (!heap.isEmpty()) {
+// //     const { log, sourceIndex } = heap.pop();
+// //     printer.print(log);
+
+// //     const nextLog = await logSources[sourceIndex].popAsync();
+// //     if (nextLog) {
+// //       heap.push({ log: nextLog, sourceIndex });
+// //     }
+// //   }
+
+// //   printer.done();
+// //   return console.log("Async sort complete.");
+// // };
+
+const FastPriorityQueue = require("fastpriorityqueue");
+
+module.exports = async (logSources, printer) => {
+  const heap = new FastPriorityQueue((a, b) => a.log.date < b.log.date);
+
+  // Fetch the first log from each source and push into the heap
+  await Promise.all(
+    logSources.map(async (source, index) => {
+      const log = await source.popAsync();
+      if (log) {
+        heap.add({ log, index, source });
+      }
+    })
+  );
+
+  while (!heap.isEmpty()) {
+    // Pop the earliest log
+    const { log, index, source } = heap.poll();
+
+    // Start fetching the next log from the same source
+    const nextLogPromise = source.popAsync();
+
+    // Print the current log
+    printer.print(log);
+
+    // Fetch and process the next log
+    const nextLog = await nextLogPromise;
+    if (nextLog) {
+      heap.add({ log: nextLog, index, source });
+    }
+  }
+
+  printer.done();
+  console.log("Async sort complete.");
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,30 @@
+// sync-sorted-merge.js
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const MinHeap = require("../lib/min-heap");
 
 module.exports = (logSources, printer) => {
+  const heap = new MinHeap((a, b) => a.log.date - b.log.date);
+
+  // Initialize the heap with the first log from each source
+  logSources.forEach((source, index) => {
+    const log = source.pop();
+    if (log) {
+      heap.push({ log, sourceIndex: index });
+    }
+  });
+
+  // Merge logs
+  while (!heap.isEmpty()) {
+    const { log, sourceIndex } = heap.pop();
+    printer.print(log);
+
+    const nextLog = logSources[sourceIndex].pop();
+    if (nextLog) {
+      heap.push({ log: nextLog, sourceIndex });
+    }
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
