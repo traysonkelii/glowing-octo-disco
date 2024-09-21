67c2f0e86a4e0e1dface1c8133a11d7750b24883
traysonkelii <traysonkelii@gmail.com>
2024-09-21 09:00:33 -0700

    improved Async time

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,69 +1,48 @@
-// "use strict";
-
-// const MinHeap = require("../lib/min-heap");
-
-// // module.exports = async (logSources, printer) => {
-// //   const heap = new MinHeap((a, b) => a.log.date - b.log.date);
-// //   const initialPromises = [];
-
-// //   // Initialize the heap with the first log from each source
-// //   logSources.forEach((source, index) => {
-// //     const promise = source.popAsync().then((log) => {
-// //       if (log) {
-// //         heap.push({ log, sourceIndex: index });
-// //       }
-// //     });
-// //     initialPromises.push(promise);
-// //   });
-
-// //   // Wait for all initial logs to be fetched
-// //   await Promise.all(initialPromises);
-
-// //   // Merge logs
-// //   while (!heap.isEmpty()) {
-// //     const { log, sourceIndex } = heap.pop();
-// //     printer.print(log);
-
-// //     const nextLog = await logSources[sourceIndex].popAsync();
-// //     if (nextLog) {
-// //       heap.push({ log: nextLog, sourceIndex });
-// //     }
-// //   }
-
-// //   printer.done();
-// //   return console.log("Async sort complete.");
-// // };
+"use strict";
 
 const FastPriorityQueue = require("fastpriorityqueue");
 
 module.exports = async (logSources, printer) => {
   const heap = new FastPriorityQueue((a, b) => a.log.date < b.log.date);
 
-  // Fetch the first log from each source and push into the heap
-  await Promise.all(
-    logSources.map(async (source, index) => {
-      const log = await source.popAsync();
-      if (log) {
-        heap.add({ log, index, source });
+  const promiseQueue = {}; // Keeps track of pending fetch promises for each source.
+  let activeSource = -1; // Indicates the source currently being processed to avoid conflicts.
+
+  // Helper function to get next log
+  async function fetchNextLog(index) {
+    const source = logSources[index];
+    const log = await source.popAsync();
+    if (log) {
+      // Places next log in proper location on the stack
+      heap.add({ log, index, source });
+    }
+    // Updates the queue of promises
+    if (log && index !== activeSource) {
+      if (!promiseQueue[index]) {
+        promiseQueue[index] = [fetchNextLog(index)];
+      } else {
+        promiseQueue[index].push(fetchNextLog(index));
       }
-    })
-  );
+    }
+  }
+
+  // Fetch the first log from each source and push into the heap
+  await Promise.all(logSources.map(async (_, index) => fetchNextLog(index)));
 
   while (!heap.isEmpty()) {
-    // Pop the earliest log
+    // Pop the earliest log and print
     const { log, index, source } = heap.poll();
-
-    // Start fetching the next log from the same source
-    const nextLogPromise = source.popAsync();
-
-    // Print the current log
     printer.print(log);
 
-    // Fetch and process the next log
-    const nextLog = await nextLogPromise;
-    if (nextLog) {
-      heap.add({ log: nextLog, index, source });
-    }
+    // Update the index of the active source
+    activeSource = index;
+
+    // Empty out the current queue of promises, concurrency control
+    await Promise.all(promiseQueue[index]);
+    activeSource = -1;
+
+    // Dynamically update the queue by adding the next log fetch promise
+    promiseQueue[index] = [fetchNextLog(index)];
   }
 
   printer.done();
